bdc3f6612b394253be4f7eedac5bf938bad86ad5
Shalini19392 <shaliniselvaraj0806@gmail.com>
2026-02-06 19:55:27 +0530

    Add files via upload

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package week1.day1;
+public class EmployeeDetails {
+public static void main(String[] args) {
+String name = "John Doe";
+int age =30;
+float salary=55000.75f;
+char gender ='M';
+Boolean acceptTerms =true;
+float rating =4.5f;
+long mobilenumber=9876543210L;
+int pincode=56001;
+byte luckyNumber=7;
+System.out.println
+("Name is:" + name + "\n" +"salary is:" + salary + "\n" + "byte is:" + age + "\n" +
+"gender is:" +gender + "\n" + "accepTerms is:" + acceptTerms + "\n" +
+"rating is:" + rating + "\n" +"mobilenumber is:" + mobilenumber + "\n" + "pincode is:" + pincode + "\n" +
+"luckyNumber is: " + luckyNumber + "\n");
+
+}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Firefox {
+
+public static void main(String[] args) {
+float browserversion =100.2f;
+String browsername ="firefox";
+boolean isVisible =true;
+int releaseyear= 1998;
+char browserlogo= 'f';
+System.out.println
+( browserversion + "\n" + browsername + "\n" + isVisible + "\n" +releaseyear +"\n"
++ browserlogo);
+
+}
+
+}
